68a57465427aea4d1dc4a199a50570602fe65f86
sayaksr <sayaksr05@gmail.com>
2023-09-08 14:29:53 -0500

    framework source init

---
+++ blocklist_measurement/blocklist_measurement.py
@@ -0,0 +1,107 @@
+import csv
+import time
+import requests
+import os
+import matplotlib.pyplot as plt
+
+# Counters for each blocklist
+phishtank_count = 0
+ecx_count = 0
+openphish_count = 0
+gsb_count = 0
+
+def download_ecx_file(temp_filename):
+    os.system(f"""
+    wget --quiet \
+      --method GET \
+      --header 'Authorization: <Enter eCX key here>' \
+      --header 'Content-Type: application/json' \
+      --output-document \
+      - 'https://api.ecrimex.net/phish?container=csv' > {temp_filename}
+    """)
+
+def check_phishtank(url):
+    global phishtank_count
+    response = requests.get(f"https://checkurl.phishtank.com/checkurl/{url}")
+    if response.json()['in_database']:
+        phishtank_count += 1
+        return True
+    return False
+
+def check_ecx(url, temp_filename):
+    global ecx_count
+    try:
+        with open(temp_filename, 'r') as ecx_file:
+            reader = csv.reader(ecx_file)
+            for row in reader:
+                if url in row:
+                    ecx_count += 1
+                    return True
+    except Exception as e:
+        print(f"Error in check_ecx: {e}")
+    return False
+
+def check_openphish(url):
+    global openphish_count
+    response = requests.get(f"https://openphish.com/feed.txt")
+    if url in response.text:
+        openphish_count += 1
+        return True
+    return False
+
+def check_gsb(url):
+    global gsb_count
+    response = requests.post("https://safebrowsing.googleapis.com/v4/threatMatches:find",
+        json={"client": {"clientId": "yourcompany", "clientVersion": "1.0"},
+              "threatInfo": {"threatTypes": ["MALWARE", "SOCIAL_ENGINEERING"],
+                             "platformTypes": ["ANY_PLATFORM"],
+                             "threatEntryTypes": ["URL"],
+                             "threatEntries": [{"url": url}]}})
+    if response.json().get('matches'):
+        gsb_count += 1
+        return True
+    return False
+
+def plot_data():
+    labels = ['PhishTank', 'ECX', 'OpenPhish', 'GSB']
+    counts = [phishtank_count, ecx_count, openphish_count, gsb_count]
+    plt.bar(labels, counts)
+    plt.xlabel('Blocklist Service')
+    plt.ylabel('Number of Detected URLs')
+    plt.title('Number of URLs Detected by Each Blocklist')
+    plt.show()
+
+def check_urls(filename, temp_filename, start_time, ecx_download_time):
+    current_time = time.time()
+
+    if current_time - ecx_download_time >= 10 * 60:  # 10 minutes in seconds
+        download_ecx_file(temp_filename)  # Download the eCrimex file
+        ecx_download_time = current_time
+
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        for row in reader:
+            url = row[0]  # Assuming URL is in the first column
+            check_phishtank(url)
+            check_ecx(url, temp_filename)
+            check_openphish(url)
+            check_gsb(url)
+
+    if current_time - start_time >= 3 * 60 * 60:  # 3 hours in seconds
+        print("Generating plot...")
+        plot_data()
+        start_time = current_time  # Update the start time for next interval
+
+    return start_time, ecx_download_time  # Return the updated times
+
+if __name__ == "__main__":
+    start_time = time.time()  # Initialize the start time for plotting
+    ecx_download_time = 0  # Initialize the start time for eCrimex download
+    filename = "input.csv"  # Replace with URL input file.
+    temp_filename = filename + "_temp.csv"
+
+    while True:
+        print("Checking URLs...")
+        start_time, ecx_download_time = check_urls(filename, temp_filename, start_time, ecx_download_time)
+        time.sleep(60)  # Sleep for 1 minute before the next iteration
---
+++ data_collection/get_twitter.py
@@ -0,0 +1,68 @@
+import subprocess
+import csv
+import time
+import os
+from urllib.parse import urlparse
+import requests
+
+# Keywords
+FWB_LIST = [
+    'weebly', 'duckdns', '000webhost', 'blogspot', 'wix', 'sites.google', 'github.io',
+    'firebase', 'square.site', 'forms.zoho', 'wordpress', 'docs.google', 'sharepoint',
+    'yolasite', 'myftp.org', 'godaddy', 'mailchimp', 'atwebpages', 'glitch.me',
+    'hpage', 'herokuapp', 'website.com', 'netlify'
+]
+
+# Resolve shortened URLs
+def resolve_url(short_url):
+    try:
+        response = requests.get(short_url, timeout=10)
+        return response.url
+    except Exception as e:
+        print(f"Error resolving URL {short_url}: {e}")
+        return short_url  # return the original if it can't be resolved
+
+# Main execution
+if __name__ == "__main__":
+    while True:
+        with open('urls.csv', 'a', newline='') as master_csvfile:
+            csvwriter = csv.writer(master_csvfile)
+            if master_csvfile.tell() == 0:
+                # Write header if the master file is empty
+                csvwriter.writerow(['source', 'id', 'url', 'active', 'time_discovered'])
+
+            for keyword in FWB_LIST:
+                print(f"Getting posts for {keyword}...")
+                
+                # Generate filenames based on epoch time
+                epoch_time = str(int(time.time()))
+                jsonl_filename = f"raw/results_{epoch_time}_{keyword}.jsonl"
+                csv_filename = f"raw/results_{epoch_time}_{keyword}.csv"
+
+                # Execute twarc2 search command
+                subprocess.run(["twarc2", "search", "--archive", f"url:{keyword}", jsonl_filename])
+
+                # Convert JSONL to CSV using twarc2 csv
+                subprocess.run(["twarc2", "csv", jsonl_filename, csv_filename])
+
+                # Read the CSV file
+                with open(csv_filename, 'r', newline='') as csvfile:
+                    csvreader = csv.DictReader(csvfile)
+                    for row in csvreader:
+                        tweet_id = row['id']
+                        link = row.get('expanded_url', '')
+                        if link:
+                            resolved_url = resolve_url(link)
+                            parsed_url = urlparse(resolved_url)
+                            if any(kw in parsed_url.netloc for kw in FWB_LIST):
+                                # Write to master CSV
+                                csvwriter.writerow([
+                                    'twitter',
+                                    tweet_id,
+                                    resolved_url,  # Include the resolved URL
+                                    'true',
+                                    epoch_time
+                                ])
+
+        print("Sleeping for 10 minutes...")
+        time.sleep(600)  # sleep for 10 minutes
